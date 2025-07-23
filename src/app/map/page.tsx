95e291c23cb209f5e40c47dc895f70052d8888d1
root <root@z0d14clegion.localdomain>
2025-07-23 00:52:44 -0500

    fix: fix container styles

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,7 +2,7 @@ import MapPageClient from './MapPageClient'
 
 export default function MapPage() {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div>
       <h1 className="text-2xl font-bold text-center mt-4">CirclePop🎈📌</h1>
       <MapPageClient />
     </div>
